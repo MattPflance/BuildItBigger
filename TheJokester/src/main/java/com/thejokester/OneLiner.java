ac0b1227ccc488fe0eb2adf175196d6a2f9784be
MattPflance <matthewpflance@gmail.com>
2016-03-22 23:36:50 -0400

    Feature: Added English jokes to library that get displayed

--- TheJokester/src/main/java/com/thejokester/OneLiner.java
+++
@@ -1,7 +0,0 @@
-package com.thejokester;
-
-public class OneLiner {
-
-
-
-}
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -3,18 +3,23 @@ package com.udacity.gradle.builditbigger;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.TextView;
+import com.thejokester.Jokester;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Jokester theJokester;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        theJokester = new Jokester();
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        ((TextView)findViewById(R.id.instructions_text_view)).setText(theJokester.getOneLiner());
     }
 
 
