27a5277c618fc4f8cfce447e608ed729273e6c09
MattPflance <matthewpflance@gmail.com>
2016-03-25 21:09:19 -0400

    Partial Feature: workings of knock knock and dad jokes for paid flavor

--- TextDisplayActivity/src/main/java/com/mattpflance/textdisplayactivity/TextDisplayActivity.java
+++ TextDisplayActivity/src/main/java/com/mattpflance/textdisplayactivity/TextDisplayActivity.java
@@ -2,17 +2,39 @@ package com.mattpflance.textdisplayactivity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class TextDisplayActivity extends AppCompatActivity {
+
+    public static final String ONE_LINER = "ONE LINER";
+    public static final String KNOCK_KNOCK = "KNOCK KNOCK";
+    public static final String DAD_JOKE = "DAD JOKE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_display);
 
-        String joke = getIntent().getStringExtra("ONE LINER");
+        // Allows the back button to be displayed
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String joke = getIntent().getStringExtra(ONE_LINER);
 
         ((TextView)findViewById(R.id.joke_text_view)).setText(joke);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int android_home_id = 16908332; // Temp until I remember what the android.id.home thing is
+
+        // Finish activity if user taps on the back button
+        if (item.getItemId() == android_home_id) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import java.util.ArrayList;
 
 import com.thejokester.Jokester;
 import com.mattpflance.textdisplayactivity.TextDisplayActivity;
@@ -20,12 +21,24 @@ public class MainActivity extends AppCompatActivity {
         mTheJokester = new Jokester();
     }
 
-    public void tellJoke(View view){
+    public void tellOneLiner(View view){
         String oneLiner = mTheJokester.getOneLiner();
         Intent intent = new Intent(this, TextDisplayActivity.class);
-        intent.putExtra("ONE LINER", oneLiner);
+        intent.putExtra(TextDisplayActivity.ONE_LINER, oneLiner);
         startActivity(intent);
     }
 
+    public void tellKnockKnock(View view){
+        ArrayList<String> knockKnock = new ArrayList<>(); //mTheJokester.getKnockKnock();
+        Intent intent = new Intent(this, TextDisplayActivity.class);
+        intent.putStringArrayListExtra(TextDisplayActivity.KNOCK_KNOCK, knockKnock);
+        startActivity(intent);
+    }
 
+    public void tellDadJoke(View view){
+        String dadJoke = "Jaborni"; //mTheJokester.getDadJoke();
+        Intent intent = new Intent(this, TextDisplayActivity.class);
+        intent.putExtra(TextDisplayActivity.DAD_JOKE, dadJoke);
+        startActivity(intent);
+    }
 }
